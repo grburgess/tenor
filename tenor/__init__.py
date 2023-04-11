8a9781faa342441dfbbc2b5c14cc8d818d7eaf43
J. Michael Burgess <jmichaelburgess@gmail.com>
2023-04-11 22:09:31 +0200

    this is the way

---
+++ tenor/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for tenor."""
+
+__author__ = """J. Michael Burgess"""
+__email__ = 'jburgess@mpe.mpg.de'
+
+
+from .utils.configuration import tenor_config, show_configuration
+from .utils.logging import update_logging_level, activate_warnings, silence_warnings
